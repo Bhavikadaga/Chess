cc14efa354ccb2e6b65443f36446a7f4f46a4132
Bhavika Daga <48dagabhavika@gmail.com>
2025-09-21 17:49:01 +0530

    chess project setup

---
+++ public/js/chessgame.js
@@ -0,0 +1,70 @@
+const socket = io(); //sends request to backend, this will runn io.on()
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
+
+let dragggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () =>{
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowindex) => {
+        row.forEach((square, squareindex) =>{
+            const squareElement = document.createElement("div");
+            squareElement.classList.add("square", 
+                (rowindex + squareindex)%2 === 0? "Light" : "dark"
+            );
+
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            if(square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add("piece", square.color === "w" ? "white" : "black");
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart", (e) =>{
+                    if(pieceElement.draggable){
+                        dragggedPiece = pieceElement;
+                        sourceSquare = {row: rowindex, col:squareindex};
+                        e.dataTransfer.setData("text/plain", "");
+                    }
+                });
+
+                pieceElement.addEventListener("dragend", (e) =>{
+                    dragggedPiece = null;
+                    sourceSquare = null;
+                });
+
+                squareElement.appendChild(pieceElement);
+            }
+
+            squareElement.addEventListener("dragover", (e) =>{
+                e.preventDefault();
+            });
+
+            squareElement.addEventListener("drop", (e) =>{
+                e.preventDefault();
+                if(dragggedPiece){
+                    const targetedSource = {
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(squareElement.dataset.col)
+                    };
+
+                    handleMove(sourceSquare, targetedSource);
+                }
+            });
+            
+            boardElement.appendChild(squareElement);
+        });
+
+    });
+}
+
+const handleMove = () =>{}
+
+const getPieceUniCode = () =>{}
+
+renderBoard();
